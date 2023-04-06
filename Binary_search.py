ae35a4314d8367e7b9434608df9d4bfbc57c4909
JOY <117473600+imjoyhalder@users.noreply.github.com>
2023-04-07 01:20:05 +0600

    Add files via upload

---
+++ Binary_search.py
@@ -0,0 +1,25 @@
+pos = -1
+def B_search(list,n):
+    l = 0
+    u = len(list)
+    global pos
+    pos = -1
+    while l<u:
+        mid = int((l+u)/2)
+        if list[mid] == n:
+            pos = mid
+            return True
+        else:
+            if list[mid] <n:
+                l = mid
+            else:
+                u = mid
+
+list = [1,2,46,2213234,3456,456,356,2345,458,3412,215434523,68734652,678346,25675234675,25723346257234]
+print(len(list))
+n = 25675234675
+if B_search(list,n):
+    print("Found ")
+    print(f"Position is {pos}")
+else:
+    print("Not Found")
